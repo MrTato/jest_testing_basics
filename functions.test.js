4d72e501e822be730169f68fe12fa4b97b2d4881
Bayardo <bjlslopez@gmail.com>
2021-09-23 13:42:46 -0600

    Use of not and comments on other functions that can be used

--- functions.test.js
+++ functions.test.js
@@ -8,3 +8,17 @@ test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
 
+/**
+ * Guarantees the return value is not the expected value
+ * Here, the function should not return 5 when passed in 2 + 2
+ */
+test('Adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTH & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
